82a862eacf55545261540aec3a8b7226431d1d27
luochenLing <864684766@qq.com>
2019-12-26 12:51:14 +0800

    V1.2.0

--- src/store/modules/travel/filterCondition.ts
+++ src/store/modules/travel/filterCondition.ts
@@ -1,17 +1,10 @@
 const state = {
-  curFilter:{
-    curTitle:'',
-    curCode:''
-  },
   curRecommend:{},
   curDateList:{},
-  curSpecialList:{},
-  showConditionPopup:false
+  curSpecialList:{}
 };
 
 const getters = {
-  getCurFilter: (state: { curFilter: object }) => state.curFilter,
-  getShowConditionPopup:(state: { showConditionPopup:boolean }) => state.showConditionPopup,
   getCurRecommend:(state: { curRecommend: object }) => state.curRecommend,
   getCurDateList:(state: { curDateList: object }) => state.curDateList,
   getCurSpecialList:(state:{curSpecialList:object})=>state.curSpecialList
@@ -22,13 +15,6 @@ const actions = {
 };
 
 const mutations = {
-  setCurFilter(state: any, curObj: any) {
-    state.curFilter.curTitle = curObj.text;
-    state.curFilter.curCode = curObj.code;
-  },
-  setShowConditionPopup(state: any, showConditionPopup: boolean) {
-    state.showConditionPopup = showConditionPopup;
-  },
   setCurRecommend(state: any, curRecommend: object) {
     state.curRecommend = curRecommend;
   },
